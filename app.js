da64b490f28135cf8deaebd56fc04cb06d8c6706
Eduardo Siquieroli Abrão e Silva <siquieroli@gmail.com>
2025-09-10 19:46:26 -0300

    Update app.js
    speech desativado

--- app.js
+++ app.js
@@ -23,14 +23,14 @@ function falarTextoDaTela(texto) {
     // responsiveVoice.speak(texto, 'Brazilian Portuguese Female', { rate: 1.2 });
 
     // Speech: Web Speech API
-    if ('speechSynthesis' in window) {
-        let utterance = new SpeechSynthesisUtterance(texto);
-        utterance.lang = 'pt-BR';
-        utterance.rate = 1.2;
-        window.speechSynthesis.speak(utterance);
-    } else {
-        console.log("Web Speech API não suportada neste navegador.");
-    }
+    // if ('speechSynthesis' in window) {
+    //     let utterance = new SpeechSynthesisUtterance(texto);
+    //     utterance.lang = 'pt-BR';
+    //     utterance.rate = 1.2;
+    //     window.speechSynthesis.speak(utterance);
+    // } else {
+    //     console.log("Web Speech API não suportada neste navegador.");
+    // }
 }
 
 function exibirMensagemInicial() {
